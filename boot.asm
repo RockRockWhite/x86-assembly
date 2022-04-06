0a2c9f951ab97cc967c63e05a1233c378dccff2a
RockRockWhite <1834309151@qq.com>
2022-04-06 18:04:18 +0800

    内中断

--- boot.asm
+++ boot.asm
@@ -8,14 +8,35 @@ mov ax, 0
 mov ss, ax
 mov sp, 0x7c00
 
-push word 0xaabb
-push word 0xccdd
-push dword 0xeeffaabb
 
-pop ax
-pop bx
-pop cx
+mov cx, 0
+; call 0:func
 
+mov word [0x80 * 4], intr
+mov word [0x80 * 4 + 0x2], 0
+
+mov word [0x0], div_err
+mov word [0x2], 0
+
+mov dx, 0
+mov ax, 5
+mov bx, 0
+div bx
+
+int 0x80
+
+jmp halt
+
+
+div_err:
+    iret
+
+intr:
+    iret
+func:
+    retf
+function:
+    dw func, 0
 halt:
     xchg bx, bx
     jmp halt
