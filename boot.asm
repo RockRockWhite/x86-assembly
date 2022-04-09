1733d1d768a19bc7a60cb9edfa7676df39fa89c6
RockRockWhite <1834309151@qq.com>
2022-04-09 10:57:48 +0800

    外中断

--- boot.asm
+++ boot.asm
@@ -5,99 +5,72 @@ int 0x10
 
 xchg bx, bx
 
-CRT_ADDR_REG equ 0x3D4
-CRT_DATA_RGU equ 0x3D5
-CRT_CURSOR_HIGH equ 0x0E
-CRT_CURSOR_LOW equ 0x0F
+PIC_M_CMD equ 0x20
+PIC_M_DATA equ 0x21
 
-mov ax, message
-call print
+; 注册中断函数
+mov word [8 * 4], clock
+mov word [8 * 4 + 2], 0
+; OCW1写入屏蔽字
+mov al, 0b1111_1110
+out PIC_M_DATA, al
+; 打开中断允许标志
+sti; IF = 1 cli
 
-xor ax, ax
+loopa:
+    mov al, 'A'
+    mov bx, 5
+    call blink
+    jmp loopa
 
+clock:
+    push ax
+    push bx
+
+    mov al, 'C'
+    mov bx, 4
+    call blink
+    xchg bx, bx
+
+    ; OCW2写入0x20表示中断完成
+    mov al, 0x20
+    out PIC_M_CMD, al
+
+    pop bx
+    pop ax
+    iret
+
+blink: ; 将al中的值闪烁输出在bx位置
+    push cx
+    push es
+
+    mov cx, 0xb800
+    mov es, cx
+
+    shl bx, 1
+
+    ; 取出当前文本值
+    mov dl, [es:bx]
+    ; 比较
+    cmp dl, ' '
+    je .set_char
+    jne .set_space
+
+    .set_char:
+        mov [es:bx], al
+        jmp .done
+    .set_space:
+        mov byte [es:bx], ' '
+        jmp .done
+    .done:
+        pop es
+        pop cx
+
+        ret
 
 halt:
     hlt ; 关闭cpu, 等待外中断
     jmp halt
-message:
-    db "Hello World!", 0
-get_cursor: ; 获得光标位置, 参数用ax返回
-    push dx
-
-    mov dx, CRT_ADDR_REG
-    mov al, CRT_CURSOR_HIGH
-    out dx, al
-
-    mov dx, CRT_DATA_RGU
-    in al, dx
-    shl ax, 8
-
-    mov dx, CRT_ADDR_REG
-    mov al, CRT_CURSOR_LOW
-    out dx, al
-
-    mov dx, CRT_DATA_RGU
-    in al, dx
-
-    pop dx
-
-    ret
-set_cursor: ; 设置光标位置函数, 参数用ax传递
-    push dx
-    push bx
-
-    mov bx, ax
-
-    mov dx, CRT_ADDR_REG
-    mov al, CRT_CURSOR_LOW
-    out dx, al
-    mov dx, CRT_DATA_RGU
-    mov al, bl
-    out dx, al
-
-
-    mov dx, CRT_ADDR_REG
-    mov al, CRT_CURSOR_HIGH
-    out dx, al
-    mov dx, CRT_DATA_RGU
-    mov al, bh
-    out dx, al
-
-    pop bx
-    pop dx
-    ret
-
-print: ;将ax寄存器地址指向的字符输出
-    mov si, ax
-
-    mov ax, 0xb800
-    mov es, ax
-print_loop:
-    push bx
-
-    call get_cursor
-    mov di, ax
-    shl di, 1
-
-    mov bl, [si]
-    cmp bl, 0
-    je print_end
-
-    mov bh, 0b1111_1010
-    mov [es:di], bx
-
-
-    inc si
-    inc ax
-
-    call set_cursor
-
-    xchg bx, bx
-
-    jmp print_loop
-print_end:
-    pop bx
-    ret
 
 times 510 - ($ - $$) db 0
 db 0x55, 0xaa
